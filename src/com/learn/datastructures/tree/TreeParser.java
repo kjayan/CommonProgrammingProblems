2eb00b598e6c49b5632e30f74c0a38c6cbe721eb
Jayan Kuttagupthan <jkuttaguptha@jkuttaguptha-m01.vmware.com>
2017-09-19 10:33:09 +0530

    Restructuring

--- src/com/learn/datastructures/tree/TreeParser.java
+++ src/com/learn/datastructures/tree/TreeParser.java
@@ -100,19 +100,64 @@ public class TreeParser {
         }
 
         Set<Map.Entry<Integer,Integer>> valueSet = valueMap.entrySet();
-        Iterator<Map.Entry<Integer,Integer>> iterator = valueSet.iterator();
-        while(iterator.hasNext()){
-            System.out.println(iterator.next().getValue());
+        for (Map.Entry<Integer, Integer> value : valueSet) {
+            System.out.println(value.getValue());
         }
 
     }
 
     private int getHeight(Node node){
-        return 0;
+        if(node == null){
+            return 0;
+        }
+        else{
+            int lHeight = getHeight(node.getLeft());
+            int rHeight = getHeight(node.getRight());
+            if(lHeight > rHeight){
+                return lHeight;
+            }
+            else{
+                return rHeight;
+            }
+        }
     }
 
+    //Level Order Traversal - O(n2) complexity
     public void topDownLevelOrderTraversal(Node node){
 
+        int height = getHeight(node);
+        for(int i=1;i<=height;i++){
+            printTopDownLevelOrderTraversal(node,i);
+        }
+
+    }
+    private void printTopDownLevelOrderTraversal(Node node, int level){
+        if(node == null){
+            return;
+        }
+        if(level == 1){
+            System.out.println(node.getData());
+        }
+        else if(level > 1) {
+            printTopDownLevelOrderTraversal(node.getLeft(), level - 1);
+            printTopDownLevelOrderTraversal(node.getRight(), level - 1);
+        }
+    }
+
+    //Level Order Traversal - O(n) complexity
+    public void printTopDownLevelOrderOptimized(Node node){
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(node);
+        while(!queue.isEmpty()){
+            Node temp = queue.remove();
+            System.out.println(temp.getData());
+            if(temp.getLeft() != null){
+                queue.add(temp.getLeft());
+            }
+            if(temp.getRight() != null){
+                queue.add(node.getRight());
+            }
+        }
     }
 
     public void topDownLevelOrderSpiralTraversal(Node node){
@@ -167,12 +212,86 @@ public class TreeParser {
 
     public void preOrderIterative(Node node){
 
+        if(node == null){
+            return;
+        }
+
+        Stack<Node> stack = new Stack<>();
+        stack.push(node);
+        while(!stack.isEmpty()){
+            Node temp = stack.peek();
+            System.out.println(temp.getData());
+            stack.pop();
+
+            if(temp.getRight() != null){
+                stack.push(temp.getRight());
+            }
+
+            if(temp.getLeft() != null){
+                stack.push(temp.getLeft());
+            }
+        }
 
     }
     public void inOrderIterative(Node node){
 
+        if(node == null){
+            return;
+        }
+        Stack<Node> stack = new Stack<>();
+        Node temp = node;
+        while(temp != null){
+            stack.push(temp);
+            temp = temp.getLeft();
+        }
+
+        while(stack.size() > 0){
+            temp = stack.pop();
+            System.out.println(temp.getData());
+
+            if(temp.getRight() != null){
+                temp = temp.getRight();
+                while(temp != null){
+                    stack.push(temp.getLeft());
+                    temp = temp.getLeft();
+                }
+            }
+        }
     }
-    public void postOrderIterative(Node node){
+    public void postOrderIterativeUsing1Stack(Node node){
+
+    }
+
+    /**
+     *
+     * @param node
+     */
+    public void postOrderIterativeUsing2Stack(Node node){
+
+        if(node == null){
+            return;
+        }
+
+        Stack<Node> stack1 = new Stack<>();
+        Stack<Node> stack2 = new Stack<>();
+
+        while(!stack1.isEmpty()){
+            Node temp = stack1.pop();
+            stack2.push(temp);
+
+            if(temp.getLeft()!= null){
+                stack1.push(temp.getLeft());
+            }
+
+            if(temp.getRight() != null){
+                stack1.push(temp.getRight());
+            }
+        }
+
+        while(!stack2.isEmpty()){
+            Node temp = stack2.pop();
+            System.out.println(temp.getData());
+        }
 
     }
 
